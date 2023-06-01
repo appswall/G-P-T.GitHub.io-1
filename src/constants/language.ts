df01590c7f629249b6eb0a7b777a29ae5854654c
You-App <54545296+You-App@users.noreply.github.com>
2023-06-01 14:57:26 +0300

    Update

--- src/constants/language.ts
+++ src/constants/language.ts
@@ -1,13 +1,17 @@
 // languages that have translation files in `public/locales`
 export const i18nLanguages = [
   'en',
+  'ru',
 ] as const;
 
 // languages that are selectable on the web page
 export const selectableLanguages = [
+  // 'en',
   'en',
+  'ru',
 ] as const;
 
 export const languageCodeToName = {
   'en': 'English',
+  'ru': 'Русский',
 };
