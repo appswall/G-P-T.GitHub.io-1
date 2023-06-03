3f4e3659cfbd10b44b92f15781deb9a1e7cdb3a2
You-App <54545296+You-App@users.noreply.github.com>
2023-06-03 16:27:16 +0300

    Update useAddChat.ts

--- src/hooks/useAddChat.ts
+++ src/hooks/useAddChat.ts
@@ -12,11 +12,11 @@ const useAddChat = () => {
     if (chats) {
       const updatedChats: ChatInterface[] = JSON.parse(JSON.stringify(chats));
       let titleIndex = 1;
-      let title = `New Chat ${titleIndex}`;
+      let title = `✍ ${titleIndex}`;
 
       while (chats.some((chat) => chat.title === title)) {
         titleIndex += 1;
-        title = `New Chat ${titleIndex}`;
+        title = `✍ ${titleIndex}`;
       }
 
       updatedChats.unshift(generateDefaultChat(title, folder));
